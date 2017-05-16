2d589f66e4b7e594bba4b15460a09423355c01ee
ravikishorethella <ravikishorethella@gmail.com>
2017-05-15 23:16:28 -0400

    DOM selectors getElementById

---
+++ selector.js
@@ -0,0 +1,11 @@
+// our main target is to come up with four different ways to select the first <p> tag
+
+
+//======================================================================================
+
+////  method 1  (getElementById)
+//// this takes a string as an argument and returns the one element with a matching ID
+//
+//var tag = document.getElementById("first");
+
+//======================================================================================
