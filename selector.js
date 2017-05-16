4beb972cfc7e9076e4d286e11d253605c95f6f42
ravikishorethella <ravikishorethella@gmail.com>
2017-05-15 23:37:33 -0400

    possible types of DOM selectors

--- selector.js
+++ selector.js
@@ -9,3 +9,52 @@
 //var tag = document.getElementById("first");
 
 //======================================================================================
+
+////  method 2  (getElementsByTagName)
+//// this method returns a list of all elements of a given tag name
+
+//var tags = document.getElementsByName("p")[0];
+
+//======================================================================================
+
+////  method 3  (getElementsByClassName)
+//// this method takes a string argument and returns a list of elements that have a matching class
+
+//var tags = document.getElementsByClassName("special")[0];
+
+//======================================================================================
+
+////  method 4  (querySelector)
+//// returns the first element that matches a given css style selector
+
+//// there are two ways of solving this:
+
+//// 1: by id
+//// 2: by class
+//// 3. by p
+
+//// by id
+//var tag = document.querySelector("#first");
+//
+//// by class
+//var tag = document.querySelector(".special");
+//
+//// by p
+//var tag = document.querySelector("p"); 
+//======================================================================================
+
+////  method 5  (querySelectorAll)
+//// returns a list of element that matches a given css style selector
+
+//var tags = document.querySelectorAll("p")[0];
+
+//======================================================================================
+
+//There are some other ways as well. If something is adjacent to something then we can use in the following way
+// in our case the first paragraph is adjacent to the h1 tag 
+// or the p tag is followed by the h1 tag
+
+//var tag = document.querySelector("h1 + p");
+
+//======================================================================================
+
